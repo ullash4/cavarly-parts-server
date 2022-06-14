770527b866d1007fa03936f59665f865beccb309
ullash4 <programmerullash@gmail.com>
2022-06-14 17:10:04 +0600

    complete pushing

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.go7gn.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-
+// Jwt funtion
 const verifyJWT=(req, res, next)=>{
   const authHeader =req.headers.authorization;
   if(!authHeader){
